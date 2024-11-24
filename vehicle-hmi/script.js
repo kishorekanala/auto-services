261e7242b96ade012439bb35541295c5b11b4e7a
Kishore Kanala <kishorekanala@gmail.com>
2024-11-24 22:03:31 +0530

    Add package.json and .npmignore files for dependency management

--- vehicle-hmi/script.js
+++ vehicle-hmi/script.js
@@ -1,15 +1,25 @@
-function toggleButtonAction() {
+const zmq = require('zeromq');
+const sock = zmq.socket('sub');
+
+sock.connect('tcp://localhost:5555');
+
+sock.on('message', function(topic, message) {
+    const status = message.toString();
     const button = document.getElementById('toggleButton');
     const statusLabel = document.getElementById('ftchmi_currentstatusvalue');
-    const fuelSwitch = document.getElementById('fuelSwitch');
-    
-    if (button.innerText === 'Open') {
-        button.innerText = 'Close';
-        statusLabel.innerText = 'Open';
-        fuelSwitch.checked = true;
-    } else {
+
+    if (status === 'close') {
         button.innerText = 'Open';
         statusLabel.innerText = 'Closed';
-        fuelSwitch.checked = false;
+    } else if (status === 'open') {
+        button.innerText = 'Close';
+        statusLabel.innerText = 'Open';
     }
+
+    // Unsubscribe after receiving the status
+    sock.unsubscribe('FUELTANKCAP_STATUS');
+});
+
+function toggleButtonAction() {
+    sock.subscribe('FUELTANKCAP_STATUS');
 }
